8492e4f384d369b97e8f4737141d96b9aa514705
calgraham7 <cgraham10@hawk.iit.edu>
2025-05-04 16:56:00 -0500

    Update ApplianceException.java

--- ApplianceException.java
+++ ApplianceException.java
@@ -1,3 +1,22 @@
-public class ApplianceException extends Exception{
+public class ApplianceException extends Exception {
 
+    // Default constructor
+    public ApplianceException() {
+        super("Appliance error occurred.");
+    }
+
+    // Constructor that accepts a custom message
+    public ApplianceException(String message) {
+        super(message);
+    }
+
+    // Constructor that accepts a message and a cause
+    public ApplianceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    // Constructor that accepts a cause
+    public ApplianceException(Throwable cause) {
+        super(cause);
+    }
 }
