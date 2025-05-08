daaf1abd2ec5a28123686e56f71074a75a372abe
Diego Fallad <dfallad@hawk.iit.edu>
2025-05-08 11:38:43 -0500

    Update NormalAppliance.java

--- NormalAppliance.java
+++ NormalAppliance.java
@@ -1,3 +1,26 @@
 public class NormalAppliance extends Appliance {
 
+
+private int normalCounter = 0;
+public NormalAppliance() {
+        super(0, null, 0, 0, 0);
+            normalCounter++;
+        }
+       
+    
+public NormalAppliance(long newLocation, String newName, int newOnWatt, int newOffWatt, double newProbability,
+double newReducePercentage) throws ApplianceException {
+        super(newLocation, newName, newOnWatt, newOffWatt, newProbability); 
+        normalCounter++;
+    }
+
+public int getNormalCounter (){
+        return normalCounter;
+    }
+
+public String toString() {
+		return  super.toString();
+	}
+
 }
+
