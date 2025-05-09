001e6e8786a06dc98b3fca9e1e4f14ce3ce95a15
calgraham7 <cgraham10@hawk.iit.edu>
2025-05-08 21:43:01 -0500

    Update NormalAppliance.java

--- NormalAppliance.java
+++ NormalAppliance.java
@@ -2,17 +2,25 @@ public class NormalAppliance extends Appliance {
 
 
 private int normalCounter = 0;
+private boolean isSmart = false;
+
 public NormalAppliance() {
         super(0, null, 0, 0, 0);
             normalCounter++;
         }
        
     
-public NormalAppliance(long newLocation, String newName, int newOnWatt, int newOffWatt, double newProbability,
+public NormalAppliance(long newLocation, String newName, int newOnWatt, int newOffWatt, double newProbability, boolean newIsSmart,
 double newReducePercentage) throws ApplianceException {
         super(newLocation, newName, newOnWatt, newOffWatt, newProbability); 
+        newIsSmart = getIsSmart();
         normalCounter++;
     }
+
+public boolean getIsSmart() {
+	return isSmart;
+}
+
 
 public int getNormalCounter (){
         return normalCounter;
@@ -23,4 +31,3 @@ public String toString() {
 	}
 
 }
-
