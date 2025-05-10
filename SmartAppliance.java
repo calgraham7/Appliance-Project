2256076e3bc175e488ee15031ac651aad609e334
Diego Fallad <dfallad@hawk.iit.edu>
2025-05-09 21:06:20 -0500

    Update SmartAppliance.java

--- SmartAppliance.java
+++ SmartAppliance.java
@@ -3,12 +3,14 @@ public class SmartAppliance extends Appliance {
     private static final double DEFAULT_REDUCE_PERCENTAGE = 1.0;
     private boolean isSmart;
 
-
-    public SmartAppliance(long newLocation, String newName, int newOnWatt, int newOffWatt, double newProbability, boolean newIsSmart, 
-            double newReducePercentage) {
-        super(newLocation, newName, newOnWatt, newOffWatt, newProbability);
+    public SmartAppliance(long location, String name, int onWatt, int offWatt,
+                      double probOn, boolean isSmart, double reducePercentage)
+        throws ApplianceException {
+    super(location, name, onWatt, offWatt, probOn);
+        if (reducePercentage < 0.0 || reducePercentage > 1.0)
+        throw new ApplianceException("Reduce percentage must be between 0.0 and 1.0");
         setIsSmart();
-        setReducePercentage(newReducePercentage);
+        setReducePercentage(reducePercentage);
     }
 
     public double getReducePercentage() {
@@ -16,27 +18,31 @@ public class SmartAppliance extends Appliance {
     }
 
     public void setIsSmart() {
-    	isSmart = true;
+        isSmart = true;
     }
+
     public void setReducePercentage(double newReducePercentage) {
         if (newReducePercentage > 1.0 || newReducePercentage < 0) {
             reducePercentage = DEFAULT_REDUCE_PERCENTAGE;
-        } 
-        else
+        } else
             reducePercentage = newReducePercentage;
     }
 
     public boolean equals(SmartAppliance x) {
-		if (this.getName().equals(x.getName()) && this.getOnWatts() == x.getOnWatts() && this.getOffWatts() == x.getOffWatts() && this.getProbOn() == x.getProbOn() && this.getReducePercentage() == x.getReducePercentage()){
-		return true;
-		}
-        else return false;
+        if (this.getName().equals(x.getName()) && this.getOnWatts() == x.getOnWatts()
+                && this.getOffWatts() == x.getOffWatts() && this.getProbOn() == x.getProbOn()
+                && this.getReducePercentage() == x.getReducePercentage()) {
+            return true;
+        } else
+            return false;
     }
+
     public boolean equals(Appliance a) {
 
         return false;
-}
-	public String toString(){
-		return  super.toString() + " Reduce%=" + reducePercentage;
-	}
+    }
+
+    public String toString() {
+        return super.toString() + " Reduce%=" + reducePercentage;
+    }
 }
